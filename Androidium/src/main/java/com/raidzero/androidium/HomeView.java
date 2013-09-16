579740bff1f47b6d6318b0b80d2746397396c369
raidzero <paul.raidzero@gmail.com>
2013-09-16 15:22:29 -0600

    Clean up, show the center highlight do highlight calculations in onDraw() so they are always up to date list items not bold, only bold when in highlight increase highlight size

--- Androidium/src/main/java/com/raidzero/androidium/HomeView.java
+++ Androidium/src/main/java/com/raidzero/androidium/HomeView.java
@@ -19,34 +19,39 @@
 package com.raidzero.androidium;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.*;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class HomeView extends ScrollView {
+
+    TextView center; // this is the center highlight
+
     // these are where the highlight section is
     private int centerTop;
     private int centerBottom;
-    private int centerSize;
     private int centerCenter;
 
-    private int viewTop; // top of list item view
-    private int viewBottom; // bottom of list item
-    private int viewSize; // size of the list item
+    // these are where the target child view is
+    private int viewTop;
+    private int viewBottom;
+    private int viewSize;
 
-    // these are corrdinates of scrolling
-    private int oldX;
-    private int newX;
-    private int oldY;
-    private int newY;
+    // new calls and sms counts
+    private int missedCalls, unreadSMS;
+
+    private boolean viewCentered; // is target child view in highlight
 
     // these are coordinates where the screen has been touched
     private int touchX;
     private int touchY;
 
-    private TextView view;
-
+    // necessary constructors
     public HomeView(Context context) {
         super(context);
     }
@@ -59,24 +64,9 @@ public class HomeView extends ScrollView {
         super(context, attrs, defStyle);
     }
 
-    @Override
-    protected int computeVerticalScrollRange() {
-        return super.computeVerticalScrollRange();
-    }
-
-    @Override
-    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-        this.oldX = oldl;
-        this.newX = l;
-        this.oldY = oldt;
-        this.newY = t;
-
-        //Log.i("Scrolling", "Y from [" + oldt + "] to [" + t + "]");
-        TextView center = HomeActivity.center;
-
-        int relativeCenterTop = center.getTop();
-        int relativeCenterBottom = center.getBottom();
-        centerSize = relativeCenterBottom - relativeCenterTop;
+    public void onDraw(Canvas canvas) {
+        center = HomeActivity.center;
+        int centerSize = center.getBottom() - center.getTop();
 
         // get absolute position
         int[] coords = new int[2];
@@ -85,6 +75,43 @@ public class HomeView extends ScrollView {
         centerTop = coords[1];
         centerBottom = centerTop + centerSize;
         centerCenter = (centerSize/2) + centerTop;
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        //Log.i("Scrolling", "Y from [" + oldt + "] to [" + t + "]");
+
+        ScrollView sv = this;
+
+        LinearLayout svChild_layout = (LinearLayout) sv.getChildAt(0);
+
+        int llChild_count = svChild_layout.getChildCount();
+
+        // loop through children
+        for (int i =0; i< llChild_count; i++) {
+            TextView child = (TextView) svChild_layout.getChildAt(i);
+            String childName = child.getText().toString();
+            if (childName.isEmpty()) {
+                continue;
+            }
+
+            viewSize = child.getBottom() - child.getTop();
+
+            int[] vCoords = new int[2];
+            child.getLocationOnScreen(vCoords);
+
+            // determine the bottom of the view
+            viewTop = vCoords[1];
+            viewBottom = viewTop + viewSize;
+
+            if (viewTop >= centerTop && viewBottom <= centerBottom) {
+                viewCentered = true;
+                child.setTypeface(null, Typeface.BOLD);
+            } else {
+                viewCentered = false;
+                child.setTypeface(null, Typeface.NORMAL);
+            }
+        }
 
         super.onScrollChanged(l, t, oldl, oldt);
     }
@@ -94,6 +121,12 @@ public class HomeView extends ScrollView {
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         this.touchX = (int) ev.getX();
         this.touchY = (int) ev.getY();
+
+        /*
+        if (touchY >= centerTop && touchY <= centerBottom) {
+            Log.i("TOUCH", "touched in center");
+        }
+        */
 
         //Log.i("HOMEVIEW", "Screen touched at [" + touchX + ", " + touchY + "]");
         return super.onInterceptTouchEvent(ev);
@@ -112,6 +145,12 @@ public class HomeView extends ScrollView {
         int relativeViewBottom = v.getBottom();
         viewSize = relativeViewBottom - relativeViewTop;
 
+        /*
+        Log.i("RELATIVEVIEW CENTER", "Top: " + relativeViewTop +
+              " Bottom: " + relativeViewBottom +
+              " Center: " + relativeViewCenter);
+        */
+
         int[] vCoords = new int[2];
         v.getLocationOnScreen(vCoords);
 
@@ -119,16 +158,9 @@ public class HomeView extends ScrollView {
         viewTop = vCoords[1];
         viewBottom = viewTop + viewSize;
 
-        if (centerTop == 0) { // recalculate the center position
-            int[] cCoords = new int[2];
-
-            TextView center = HomeActivity.center;
-            center.getLocationOnScreen(cCoords);
-
-            centerTop = cCoords[1];
-            centerBottom = centerTop + centerSize;
-            centerCenter = (centerSize/2) + centerTop;
-        }
+        Log.i("CENTER", "Top: " + centerTop +
+                " Bottom: " + centerBottom +
+                " Center: " + centerCenter);
 
         if (viewTop >= centerTop && viewBottom <= centerBottom) {
             //Log.i("Scrolling", v.getText().toString() + " has entered the center!");
